326074b90dec57ed740fe00d65422c3aea4bb83f
LucasFerraro <ferrarolucasmartin@gmail.com>
2023-07-22 14:46:51 -0300

    base URL changed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://ferrarolucasmartin.github.io/portfolio-v1.0/"
 })
